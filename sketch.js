ff72a1619dc2b697d66e2f10769a20a8e9784f3f
edwedja <edwedja_lima@hotmail.com>
2022-04-08 02:12:26 -0300

    assets

--- sketch.js
+++ sketch.js
@@ -7,18 +7,18 @@ var fuels, powerCoins, obstacles, obstacle1Image, obstacle2Image; // aula 40
 var cars = [], lifeImage, blastImage;
 
 function preload() {
-  backgroundImage = loadImage("./assets/planodefundo.png");
-  car1_img = loadImage("../assets/car1.png");
-  car2_img = loadImage("../assets/car2.png");
-  track = loadImage("../assets/pista.png");
+  backgroundImage = loadImage("assets/planodefundo.png");
+  car1_img = loadImage("assets/car1.png");
+  car2_img = loadImage("assets/car2.png");
+  track = loadImage("assets/pista.png");
 
-  fuelImage = loadImage("./assets/fuel.png");
-  powerCoinImage = loadImage("./assets/goldCoin.png");
+  fuelImage = loadImage("assets/fuel.png");
+  powerCoinImage = loadImage("assets/goldCoin.png");
 
-  obstacle1Image = loadImage("./assets/obstacle1.png");
-  obstacle2Image = loadImage("./assets/obstacle2.png");
-  lifeImage = loadImage("./assets/life.png");
-  blastImage = loadImage("./assets/blast.png");
+  obstacle1Image = loadImage("assets/obstacle1.png");
+  obstacle2Image = loadImage("assets/obstacle2.png");
+  lifeImage = loadImage("assets/life.png");
+  blastImage = loadImage("assets/blast.png");
 }
 
 function setup() {
